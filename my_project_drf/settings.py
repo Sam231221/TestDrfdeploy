8d93203a970c224d6d3df10cbc4f7990e16f07d9
Sameer Shahi Thakuri (Sam) <70689865+Sam231221@users.noreply.github.com>
2025-01-08 14:55:48 +0545

    allowed host updated

--- my_project_drf/settings.py
+++ my_project_drf/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xldzuh2fbo9(-_1g%#m08pr3_h$b*bvul8dcq(^8rf0u#$q%f-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "testdrfdeploy-production.up.railway.app"]
 
 
 # Application definition
